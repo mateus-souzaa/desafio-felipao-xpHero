33c0b676c71553ed853e18bb145804c77df88a8b
mateus-souzaa <mateus-souzaa@outlook.com>
2023-11-08 22:04:19 -0400

    primeiro projeto

---
+++ index.js
@@ -0,0 +1,55 @@
+let nomeHero = "Felipao"
+let xp = 500
+
+// Variaveis dos niveis do Hero
+let = ferro = 1000
+let bronze = 2000
+let prata = 5000
+let ouro = 6000
+let platina = 8000
+let ascedente = 9000
+let imortal = 10000
+let radiante = 10001
+
+// codigo de logica
+
+
+if(xp >= ferro && xp < bronze){
+    console.log("O Heroi " + nomeHero + " esta no nivel ferro, seu xp é " + ferro)
+}
+if(xp >= bronze && xp < prata){
+    console.log("O Heroi " + nomeHero + " esta no nivel bronzeseu xp é " + bronze)
+}
+if(xp >= prata && xp < ouro){
+    console.log("O Heroi " + nomeHero + " esta no nivel prata seu xp é " + prata)
+}     
+if(xp >= ouro && xp < platina){
+    console.log("O Heroi " + nomeHero + " esta no nivel ouro seu xp é " + ouro)
+}     
+if(xp >= platina && xp < ascedente){
+    console.log("O Heroi " + nomeHero + " esta no nivel platina seu xp é " + platina)
+}     
+if(xp >= ascedente && xp < imortal){
+    console.log("O Heroi " + nomeHero + " esta no nivel ascedente seu xp é " + ascedente)
+}     
+if(xp >= imortal && xp < radiante){
+    console.log("O Heroi " + nomeHero + " esta no nivel imortal seu xp é " + imortal)
+}
+if(xp >= radiante){
+    console.log("O Heroi " + nomeHero + " esta no nivel radiante seu xp é " + radiante)
+}
+if(xp < ferro){
+console.log("O Heroi " + nomeHero + " Heroi não tem nivel suficiente seu xp é " + xp)     
+}
+
+
+
+
+
+
+
+
+
+
+
+
